83def0511faa11858ebd471cf48297a69fe377e4
Zhenya Aleksanyan <alexanyan.jenya@gmail.com>
2025-07-09 20:33:04 +0400

    0 handling

--- parsing.c
+++ parsing.c
@@ -15,18 +15,18 @@ int	ft_atoi(const char *str)
 	while (is_space(str[i]))
 		i++;
 	if (str[i] == '-' || str[i] == '\0')
-		return (0);
+		return (-1);
 	if (str[i] == '+')
 		i++;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		res = res * 10 + (str[i] - '0');
 		if (res > 2147483647)
-			return (0);
+			return (-1);
 		i++;
 	}
 	if (str[i] != '\0')
-		return (0);
+		return (-1);
 	return (res);
 }
 
@@ -41,9 +41,9 @@ int	parsing(int argc, char *argv[], t_args *args)
 	args->number_of_times_each_philosopher_must_eat = -1;
 	if (argc == 6)
 		args->number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
-	if (args->number_of_philosophers <= 0 || args->time_to_die <= 0
-		|| args->time_to_eat <= 0 || args->time_to_sleep <= 0 || (argc == 6
-			&& args->number_of_times_each_philosopher_must_eat <= 0))
+	if (args->number_of_philosophers < 0 || args->time_to_die < 0
+		|| args->time_to_eat < 0 || args->time_to_sleep < 0 || (argc == 6
+			&& args->number_of_times_each_philosopher_must_eat < 0))
 	{
 		write(1, "Error: invalid arguments\n", 25);
 		return (0);
